dc94863ac7f19a11b0bf1b4b8db8e41cb47562a9
sahar75 <sahar.sweetie46@gmail.com>
2024-09-16 14:48:02 +0330

    fix run bug

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Nfc from "../../components/Nfc";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
       <Nfc />
     </View>
   );
